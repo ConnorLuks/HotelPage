fc06bea250c109a792b3fb1bd948093f7ab95642
Connor_OHalloran <HCnamehere@protonmail.com>
2024-06-04 20:24:31 -0400

    added more visual text

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const db = require('./models');
 const app = express();
 
 //SET view to Express
-app.set('view.engine', 'ejs');
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+//updated to serve files that are Static
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
