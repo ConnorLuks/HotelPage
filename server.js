71a4f0a470530c89ae282c474d9dba2623345c25
Connor_OHalloran <HCnamehere@protonmail.com>
2024-06-13 21:21:45 -0400

    edits

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const path = require('path');
-const app = expres();
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middle Ware
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true})); //bodyParser no longer used in Express
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Views
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Controlles
+// Controlls
 const hotelController = require('./controllers/hotelController');
 app.use('/', hotelController);
 
 app.listen(PORT, () => {
-    console.log('Server is running on port ${PORT}');
+    console.log(`Server is running on port ${PORT}`);
 });
